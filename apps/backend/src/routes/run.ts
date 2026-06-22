797ab5bd973476023b1313fc52c9cc0c8dee120c
Kame65 <kamedatatsuki@kamedatatsukinoMacBook-Air.local>
2026-06-22 10:09:29 +0900

    packages/shared-type, 参照ようにindex.ts, 実際の型run.tsを作成, 型共有を@cpt/sharedtypesでできるようにrootDirにpackagesを作成, backendで型参照に変更

--- apps/backend/src/routes/run.ts
+++ apps/backend/src/routes/run.ts
@@ -1,13 +1,20 @@
 import { Hono } from 'hono'
+import type { RunRequest, RunResponse } from '@cpt/shared-types'
+
+
 
 
 
 export const runRoute = new Hono();
 
+
+
 // この関数が呼ばれるのはapi/runの時のみ
 runRoute.post('/', async (c) => {
-  const body = await c.req.json()
+  const body = await c.req.json<RunRequest>()
   console.log("in runRoute Object!")
+  console.log("language = ", body.language)
+  console.log("sourceCOde = ", body.sourceCode)
   return c.json({
     received: body,
   })
