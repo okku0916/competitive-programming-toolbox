1dc165f95afd0a8f91951ace9cbe429d205fc2b5
migen223 <kose_25@icloud.com>
2026-07-14 12:09:27 +0900

    [add] 与えられたJson形式からランダムな数字を文字列で返す関数を実装しました。

--- apps/backend/src/features/random-generator/generator.ts
+++ apps/backend/src/features/random-generator/generator.ts
@@ -1,14 +1,46 @@
 
 
 import type { ListConstraint, ScalarConstraint } from "@cpt/shared-types";
+import { listenerCount } from "process";
 
 
 export function generateScalar(constraint: ScalarConstraint){
-
-    return "3";
+    let kind:string=constraint.kind;
+    let typename:string=constraint.typeName;
+    let name:string=constraint.name;
+    let min:number=constraint.min;
+    let max:number=constraint.max;
+    let options:string[]=constraint.options;
+    let res:number=0;
+    let even_odd:string="";
+    if (max<min) throw new Error("max is less than min");
+    for (let i=0;i<options.length;i++){
+        if (options[i]=="odd" || options[i]=="Odd") even_odd="o";
+        else if (options[i]=="even"||options[i]=="Even") even_odd="e";
+    }
+    if (typename=="int"){
+        if (even_odd==""){
+            res=Math.floor(Math.random()*(max-min+1)+min);
+        }else if (even_odd=="o"){
+            let range:number;
+            if (Math.abs(min)%2==0) range=Math.floor((max-min+1)/2);
+            else range=Math.floor((max-min)/2)+1;
+            res=(Math.floor(Math.random()*range)+Math.floor(min/2))*2+1;
+        }else if (even_odd=="e"){
+            let range:number;
+            if (Math.abs(min)%2==0) range=Math.floor((max-min)/2)+1;
+            else range=Math.floor((max-min+1)/2);
+            res=(Math.floor(Math.random()*range)+Math.floor((min+1)/2))*2;
+        }
+    }else{
+        if (even_odd!="") throw new Error("Even and odd cannot be defined in floating point numbers ")
+        res=Math.random()*(max-min)+min;
+    }
+    return res.toString();
 }
 
 export function generateList(constraint: ListConstraint){
+    constraint.
     return "4 3 1";
 
 }
