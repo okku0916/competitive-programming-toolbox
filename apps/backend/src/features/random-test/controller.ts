af1e20c3efda116068e0fb4ec4de30d2327f7b05
Kame65 <kamedatatsuki@kamedatatsukinoMacBook-Air.local>
2026-07-14 09:50:49 +0900

    feat: WAとなるrandom入力の探索をする機能の土台を作成

---
+++ apps/backend/src/features/random-test/controller.ts
@@ -0,0 +1,49 @@
+// このファイルではHTTPの入出力処理を行う
+// routesから呼び出される
+
+import type { Context } from 'hono'
+import { randomTester } from './randomTester.js';
+type ScalarConstraint = {
+  kind: "scalar";
+  typeName: string;
+  name: string;
+  min: number;
+  max: number;
+  options: string[];
+};
+
+type Length =
+  | { kind: "number"; value: number }
+  | { kind: "variable"; name: string };
+
+type ListConstraint = {
+    kind: "list";
+    elementType: string;
+    name: string;
+    length: Length;
+    min: number;
+    max: number;
+    options: string[];
+  };
+  
+type Constraint = ScalarConstraint | ListConstraint;
+type randomTestRequest = {
+    sourceCode: string
+    answerCode: string
+    input: Constraint
+    
+}
+
+// 型ContextはAPI通信の中身が全て入ったもの
+export async function randomTestController(c: Context) {
+
+  // awatでjson()が終わるのを待つ
+  // c.reqでcのrequestを受け取る
+  // .jsonでjson型の者を受け取れる
+  const body = await c.req.json<randomTestRequest>()
+
+  const result = await randomTester(body)
+
+  return c.json(result)
+}
+
